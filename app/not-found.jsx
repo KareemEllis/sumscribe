d6cb38dd3437ef1d6942d0db2e67db368ee80abf
Kareem Ellis <kareemellis55@gmail.com>
2024-02-06 21:48:02 -0500

    Updated not-found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 export default function NotFound() {
     return (
-        <main>
-            <h2>
-            There was a problem.
-            </h2>
-            <p>We could not find the page you were looking for.</p>
-            <p>Go back to the <Link href={'/dashboard'}>Dashboard</Link></p>
-        </main>
+        <Container component='main' maxWidth="lg" sx={{ textAlign: 'center', mt: 8 }}>
+            <Typography variant="h1" fontSize={180} fontWeight='bold' color={'primary'}>
+                404
+            </Typography>
+            <Image 
+                src={'/SumscribeLogo.png'} 
+                alt={'Sumscribe Logo'} 
+                width={64} 
+                height={64}
+                style={{ 
+                    margin: 'auto',
+                    marginBottom: 16
+                }}
+            />
+            <Typography variant="h4" fontWeight='bold' sx={{ mb: 2 }}>
+                Page Not Found
+            </Typography>
+            <Typography variant="p" >
+            We could not find the page you were looking for.
+            </Typography>
+
+            <Box sx={{ mt: 2 }}>
+                <Button variant="contained">
+                    <Link href='/dashboard'>Back to Dashboard</Link>
+                </Button>
+            </Box>
+        </Container>
     )
 }
