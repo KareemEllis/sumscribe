beaf72f2ba47dcc324f89481980317957cae8cba
Kareem Ellis <kareemellis55@gmail.com>
2024-02-10 22:03:26 -0500

    Finished other loading components

--- app/(dashboard-pages)/scribes/edit/[id]/loading.jsx
+++ app/(dashboard-pages)/scribes/edit/[id]/loading.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+import Skeleton from '@mui/material/Skeleton'
+
 export default function loading() {
     return (
-        <div>loading</div>
+        <Container component='main' maxWidth="lg">
+            <Typography variant='h5' gutterBottom>
+                <Skeleton />
+            </Typography>
+            <Typography variant='body1'  gutterBottom>
+                <Skeleton />
+            </Typography>
+            
+            <Skeleton height={50} width={250}/>
+
+            <Skeleton height={250} width={350}/>
+
+            <Skeleton height={50} width={100}/>
+
+            <Skeleton height={100} width={150}/>
+
+            
+
+        </Container>
     )
 }
--- app/(dashboard-pages)/scribes/new/loading.jsx
+++ app/(dashboard-pages)/scribes/new/loading.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
+import Container from '@mui/material/Container'
+import Skeleton from '@mui/material/Skeleton'
+import Typography from '@mui/material/Typography'
+import Input from '@mui/material/Input'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+
 export default function loading() {
     return (
-        <div>loading</div>
+        <Container component='main' maxWidth="lg">
+            <Typography variant="h5" fontWeight='bold'>
+                <Skeleton />
+            </Typography>
+
+            <Skeleton>
+                <TextField  
+                    sx={{maxWidth: '500px', width: '100%', marginTop: 4 }}
+                />
+            </Skeleton>
+           
+            <Skeleton>
+                <Input
+                    type="file"
+                    sx={{ display: 'block', maxWidth: '500px', marginTop: 4 }}
+                />
+            </Skeleton>
+
+            <Button 
+                sx={{ marginTop: 4 }}
+            >
+                <Skeleton />
+            </Button>
+        </Container>
     )
 }
